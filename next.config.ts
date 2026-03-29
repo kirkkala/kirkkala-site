85a5d1c4d31366b28028333d3b47cfe9c649b039
Timo Kirkkala <timo.kirkkala@digitalistgroup.com>
2026-03-29 17:13:15 +0300

    Inline CSS with to avoid render-blocking stylesheet requests

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Inline CSS with HTML to avoid render-blocking stylesheet requests.
+  experimental: {
+    inlineCss: true,
+  },
   images: {
     // When developing, Sharp-backed optimization for many large originals can peg CPU/RAM.
     // Hence optimization only for production builds.
